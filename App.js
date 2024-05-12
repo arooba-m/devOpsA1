e4c9b554cdcb7b7513219f40adf8cc8b53ace138
k200213am <k200213@nu.edu.pk>
2024-05-12 09:53:00 +0500

    certifications page

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import ProjectsPage from './components/ProjectsPage';
-import EducationPage from './components/EducationPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import ProjectsPage from "./components/ProjectsPage";
+import EducationPage from "./components/EducationPage";
+import CertificationPage from './components/CertificationPage';
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar/>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/education" element={<EducationPage />} />
-          
+          <Route path="/certifications" element={<CertificationPage />} />
         </Routes>
       </div>
     </Router>
